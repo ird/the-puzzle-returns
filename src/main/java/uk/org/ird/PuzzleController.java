66ef966f607de955312aa3a0e090e4741a35e395
ird <31051194+ird@users.noreply.github.com>
2017-12-30 15:27:22 +0000

    New controller logic for KnightPuzzle

--- src/main/java/uk/org/ird/PuzzleController.java
+++ src/main/java/uk/org/ird/PuzzleController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 @Controller
@@ -11,13 +12,31 @@ public class PuzzleController {
 
     @RequestMapping("/puzzle")
     public String puzzle(
-        @RequestParam(value="q", required=false) Integer q,
-        @RequestParam(value="a1", required=false) Integer a1,
-        @RequestParam(value="a2", required=false) Integer a2,
-        Model model) {
-        model.addAttribute("q", q);
-        model.addAttribute("a1", a1);
-        model.addAttribute("a2", a2);
+            @RequestParam(value="token", required=false) Integer token,
+            @RequestParam(value="answer", required=false) String answer,
+            Model model) {
+
+        Puzzle puzzle = new KnightPuzzle();
+        String message = "";
+
+        if(answer != null) { // an answer has been submitted by the player
+            if(puzzle.verify(answer, token)) {
+                return "winner";
+            }
+            message = "Bzzzt. Nope (or too slow!)";
+        }
+
+        if(token == null) { // first time player, generate new random token
+            token = ThreadLocalRandom.current().nextInt();
+        }
+
+        puzzle.generate(token);
+
+        model.addAttribute("puzzle", puzzle.toString());
+        model.addAttribute("timer", puzzle.getTimeRemaining());
+        model.addAttribute("message", message);
+        model.addAttribute("token", token);
+        model.addAttribute("answer", answer);
 
         return "puzzle";
     }
