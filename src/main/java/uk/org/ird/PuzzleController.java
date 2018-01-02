986cd598f0f1fc844e2b43bd20cd46fcb744335c
ird <31051194+ird@users.noreply.github.com>
2018-01-02 14:19:28 +0000

    Updated PuzzleController Logic

--- src/main/java/uk/org/ird/PuzzleController.java
+++ src/main/java/uk/org/ird/PuzzleController.java
@@ -1,15 +1,18 @@
 package uk.org.ird;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.concurrent.ThreadLocalRandom;
 import java.math.BigInteger;
 
 
 @Controller
 public class PuzzleController {
+    @Autowired
+    private PuzzleRepository puzzleRepository;
+    public final int MAX_PUZZLES = 100;
 
     @RequestMapping("/puzzle")
     public String puzzle(
@@ -17,40 +20,44 @@ public class PuzzleController {
             @RequestParam(value="answer", required=false) String answer,
             Model model) {
 
-        Puzzle puzzle;
-        String message = "";
+        //TODO: If MAX_PUZZLES is reached, clean up all puzzles and reset progress
 
+        Puzzle puzzle = null;
+        String message = "";
         if (token == null) {
-            // construct new puzzle with random token
             puzzle = new KnightPuzzle(5); // 5 rounds remaining
             token = puzzle.getToken();
         } else {
-            // TODO: old token supplied, retrieve puzzle
+            // old token supplied, retrieve puzzle instance (progress)
+            for(Puzzle p : puzzleRepository.findAll()) {
+                if(token == p.getToken()){
+                    puzzle = p;
+                    break;
+                }
+            }
+            // If token isn't found, create a new puzzle and token
+            if(puzzle == null){
+                puzzle = new KnightPuzzle(5);
+                token = puzzle.getToken();
+            }
             if(answer != null) { // an answer has been submitted by the player
                 if(puzzle.verify(answer)) {
-                    int rounds = puzzle.getRoundsRemaining();
-                    if(rounds == 1) {
+                    if(puzzle.decreaseAndGetRoundsRemaining() == 0)
                         return "winner";
-                    }
-                    puzzle.setRoundsRemaining(rounds - 1);
                     message = "Next round!";
-
                 } else {
                     message = "Bzzt. Go again";
-                    puzzle.setRoundsRemaining(5);
-
                 }
             }
         }
 
-        // TODO: generate new puzzle
         puzzle.generate();
 
         model.addAttribute("puzzle", puzzle.toString());
         model.addAttribute("timer", puzzle.getTimeRemaining(token));
         model.addAttribute("message", message);
         model.addAttribute("token", token);
-        // model.addAttribute("answer", answer);
+
         return "puzzle";
     }
 }
