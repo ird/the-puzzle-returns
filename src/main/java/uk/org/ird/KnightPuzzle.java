6c141edcc86a899dedd6ef58fed4d195ce044868
ird <31051194+ird@users.noreply.github.com>
2018-01-07 15:57:45 +0000

    KnightPuzzle generate() logic completed

--- src/main/java/uk/org/ird/KnightPuzzle.java
+++ src/main/java/uk/org/ird/KnightPuzzle.java
@@ -2,6 +2,9 @@ package uk.org.ird;
 
 import java.math.BigInteger;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.data.annotation.Id;
 
@@ -19,14 +22,14 @@ public class KnightPuzzle implements Puzzle {
     private final BigInteger token;
     private int roundsRemaining = 5;
     private final long timeAllowedPerRound[] = {0, 2, 4, 8, 16, 32}; // eg 16s allowed when there are 4 rounds left
-    private Character[][] puzzle;
+    private List<Character> puzzle;
     private String startTime;
     private String answer;
 
     public KnightPuzzle(int roundsRemaining) {
         token = new BigInteger(16, ThreadLocalRandom.current());
         this.roundsRemaining = roundsRemaining; // number of times the puzzle must be solved to pass
-        puzzle = new Character[8][8];
+        puzzle = null;
         startTime = null;
         answer = null;
     }
@@ -38,8 +41,50 @@ public class KnightPuzzle implements Puzzle {
     public void generate() {
         // TODO
         startTime = Instant.now().toString();
-        answer = "WONGA";
+        answer = "NAZDO"; // TODO
 
+        while(true) {
+            puzzle = Arrays.asList(new Character[64]);
+            int pos = (int)(Math.random()*63);
+            // Randomly select a start point and enter the first letter
+            puzzle.set(pos, answer.charAt(0));
+            // For each subsequent letter; work out all possible next positions (max 8),
+            // select one at random and place a letter
+            List<Integer> nextMoves = null;
+            for(int i=1; i<answer.length(); i++) {
+                nextMoves = possibleKnightPositions(pos, puzzle);
+                if(nextMoves.size() == 0)
+                    break;
+                pos = choose(nextMoves);
+                puzzle.set(pos, answer.charAt(i));
+            }
+            if(nextMoves.size() == 0)
+                continue;
+            break;
+        }
+
+        // fill in the blanks
+        Character[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        for(int i=0; i<puzzle.size(); i++)
+            if(puzzle.get(i) == null)
+                puzzle.set(i, letters[(int)(Math.random()*(letters.length - 1))]);
+
+    }
+    private static List<Integer> possibleKnightPositions(int startPos, List<Character> grid) {
+        ArrayList<Integer> res = new ArrayList<>();
+        List<Integer> relPos = Arrays.asList(-17, -15, -10, -6, 6, 10, 15, 17);
+        for(int adjustment : relPos){
+            int pos = startPos + adjustment;
+            if(pos >= 0 && pos < 64 && grid.get(pos) == null)
+                res.add(pos);
+        }
+        System.out.println("Possible positions from startPos=" + startPos + " : " + res);
+        return res;
+    }
+
+    private static int choose(List<Integer> L) {
+        return L.get((int)(Math.random()*L.size()));
     }
     /**
      * Returns a HTML representation of the current puzzle
